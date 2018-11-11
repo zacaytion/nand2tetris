14ee0cac4b930eefa8eaffdaf584d9156fe0c3bd
Zach Harris <z@zharr.is>
2018-11-11 17:46:05 -0500

    refactor later

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,70 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//Store bottom screen address
+@SCREEN
+D=A
+@i
+M=D-1
+
+//Store top screen address 
+@KBD
+D=A
+@j
+M=D
+
+//Counter for current pixel
+@i
+D=M
+@k
+M=D
+
+(LOOP) 
+@KBD
+D=M
+//Color the next pixel white if no key is pressed
+@COLOR_WHITE
+D;JEQ
+@COLOR_BLACK
+0;JMP
+
+
+(COLOR_BLACK) 
+//check if we have reached the top of the screen
+@j
+D=M
+@k
+D=D-M
+@LOOP
+D;JEQ
+
+//Increment current pixel and blacken
+@k
+A=M
+M=-1
+@k
+M=M+1
+
+@LOOP
+0;JMP
+
+(COLOR_WHITE)
+//check if we have reached the bottom of the sceen
+@i
+D=M
+@k
+D=D-M
+@LOOP
+D;JEQ
+
+//Decrement current pixel and whiten
+@k
+M=M-1
+A=M
+M=0
+
+@LOOP
+0;JMP
+
+
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Initialize R2 to 0
+@R2
+M=0
+
+// If R0 == 0, R0 * R1 == 0
+@R0
+D=M
+@END
+D;JEQ
+
+(ADDLOOP)
+  // If R1 == 0, end loop
+  @R1
+  D=M
+  @END
+  D;JEQ
+
+  // R2 = R0 + R2
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  // Subtract one from counter - R1
+  @R1
+  M=M-1
+
+  @ADDLOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
+
